9a96c12225ea6686a0bf978667e81430f68c172e
Andrey <andrei_2001pet@mail.ru>
2025-10-07 02:45:15 +0500

    готовое переделанное тестовое

--- backwards.php
+++ backwards.php
@@ -1,24 +1,40 @@
-<?
-$words_and_letter_arr = ['Cat', 'Мышь', 'houSe', 'esuOh', 'кимОД', 'third-part , fourth-part', 'can`t'];
-function backwards($arr)
+<?php 
+$capital = [];
+function processString($str)
 {
-    $result = [];
-    foreach ($arr as $words) {
-        $result[$words] = strrev($words);
+  $len = mb_strlen($str);
+  $reversed = '';
+  for ($i = 0; $i < $len; $i++) {
+    $char = mb_substr($str, $i, 1);
+    if (preg_match('/\p{Lu}/u', $char)) {
+      $index = $len - $i - 1;
+      $capital[$index] = [''];
     }
-    return $result;
-}
-//тест
-$backwards = backwards($words_and_letter_arr);
-foreach ($backwards as $test_word => $backward_word) {
-    if (strrev($backward_word) == $test_word) {
-        echo '<pre>';
-        echo 'слово ' . $test_word . ' правильно поменяло порядок букв - ' . $backward_word;
-        echo '</pre>';
+  }
+  for ($i = $len - 1; $i >= 0; $i--) {
+    $char = mb_substr($str, $i, 1);
+    if (isset($capital[$i])) {
+      $reversed .= mb_strtoupper($char);
     } else {
-        echo '<pre>';
-        echo 'слово ' . $test_word . ' не правильно поменяло порядок букв - ' . $backward_word;
-        echo '</pre>';
+      $reversed .= mb_strtolower($char);
     }
+  }
+  return $reversed;
 }
+$words_and_letter_arr = ['Cat', 'Мышь', 'houSe', 'esuOh', 'кимОД', 'third-part , fourth-part', 'can`t'];
+$test_arr = [];
+foreach ($words_and_letter_arr as $normal_word) {
+  $reversed_word = processString($normal_word);
+  $check_normal_word =  processString($reversed_word);
+  if($check_normal_word == $normal_word) {
+    echo '<pre>';
+    echo 'Слово - '. $normal_word . ' правильно поменяло порядок букв - '. $reversed_word;
+    echo '</pre>';
+  }else {
+    echo '<pre>';
+    echo 'Слово - '. $normal_word . ' не правильно поменяло порядок букв - '. $reversed_word;
+    echo '</pre>';
+  }
+}
+
 ?>
